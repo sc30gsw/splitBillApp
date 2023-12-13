77e371987a6f25a9d25b56166526b4c1f0a3f26d
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-12-13 16:56:52 +0900

    💄 Storyの作成

---
+++ frontend/src/components/CreateGroupForm/CreateGroupForm.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import CreateGroupForm from './CreateGroupForm'
+
+const meta = {
+  title: 'CreateGroupForm',
+  component: CreateGroupForm,
+} as Meta<typeof CreateGroupForm>
+
+export default meta
+
+type Story = StoryObj<typeof CreateGroupForm>
+
+export const Default: Story = {
+  args: {
+    onSubmit: async () =>
+      new Promise((resolve) => setTimeout(() => resolve(), 1000)),
+  },
+}
